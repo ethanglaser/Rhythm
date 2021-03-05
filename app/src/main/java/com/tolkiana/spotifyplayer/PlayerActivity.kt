3abab6d66d9e73a236c7e66cac7aa4fd744f7c33
ethanglaser <glasere@purdue.edu>
2021-03-05 13:25:59 -0500

    Basic app functionality complete

---
+++ app/src/main/java/com/tolkiana/spotifyplayer/PlayerActivity.kt
@@ -0,0 +1,76 @@
+package com.tolkiana.spotifyplayer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_player.*
+
+class PlayerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+        setupViews()
+        setupListeners()
+        val name = "rock.json"
+        SpotifyService.jsonHandler(name)
+        //SpotifyService.suscribeToChanges()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        SpotifyService.disconnect()
+    }
+
+    private fun setupViews () {
+        SpotifyService.getCurrentTrackImage {
+            trackImageView.setImageBitmap(it)
+        }
+
+        SpotifyService.playingState {
+            when(it) {
+                PlayingState.PLAYING -> showPauseButton()
+                PlayingState.STOPPED -> showPlayButton()
+                PlayingState.PAUSED -> showResumeButton()
+            }
+        }
+    }
+
+    private fun setupListeners() {
+        playButton.setOnClickListener {
+            SpotifyService.play("spotify:album:5L8VJO457GXReKVVfRhzyM")
+            showPauseButton()
+        }
+
+        pauseButton.setOnClickListener {
+            SpotifyService.pause()
+            showResumeButton()
+        }
+
+        resumeButton.setOnClickListener {
+            SpotifyService.resume()
+            showPauseButton()
+        }
+
+        SpotifyService.suscribeToChanges()
+    }
+
+    private fun showPlayButton() {
+        playButton.visibility = View.VISIBLE
+        pauseButton.visibility = View.GONE
+        resumeButton.visibility = View.GONE
+    }
+
+    private fun showPauseButton() {
+        playButton.visibility = View.GONE
+        pauseButton.visibility = View.VISIBLE
+        resumeButton.visibility = View.GONE
+    }
+
+    private fun showResumeButton() {
+        playButton.visibility = View.GONE
+        pauseButton.visibility = View.GONE
+        resumeButton.visibility = View.VISIBLE
+    }
+}
---
+++ app/src/main/java/com/tolkiana/spotifyplayer/Song.kt
@@ -0,0 +1,9 @@
+package com.tolkiana.spotifyplayer
+
+data class Song(
+    val album: String,
+    val artist: String,
+    val duration: Int,
+    val name: String,
+    val tempo: Double
+)
